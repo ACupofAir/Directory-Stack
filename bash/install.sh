4347c387f29351ad0df59a90a9a111cde2f807a5
June Wang <thoughts.times@gmail.com>
2023-04-21 14:52:34 +0800

    update install script

--- bash/install.sh
+++ bash/install.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/bash
 
-if [! -d "$HOME/bin" ]; then
+if [ ! -d "$HOME/bin" ]; then
     mkdir $HOME/bin
 fi
 
 curl https://raw.githubusercontent.com/ACupofAir/Directory-Stack/main/bash/dir_stack.sh > $HOME/bin/dir_stack.sh
 
-if [ -f "~/.bashrc" ]; then
-    echo "PATH="$HOME/bin:$PATH" >> ~/.bashrc
+if [ -f "$HOME/.bashrc" ]; then
+    echo "PATH=$HOME/bin:$PATH" >> ~/.bashrc
     echo "source $HOME/bin/dir_stack.sh" >> ~/.bashrc
-    source ~/.bashrc
-elif [ -f "~/.zshrc" ]; then
-    echo "PATH="$HOME/bin:$PATH" >> ~/.zshrc
+    source $HOME/.bashrc
+elif [ -f "$HOME/.zshrc" ]; then
+    echo "PATH=$HOME/bin:$PATH" >> ~/.zshrc
     echo "source $HOME/bin/dir_stack.sh" >> ~/.zshrc
-    source ~/.zshrc
+    source $HOME/.zshrc
+fi
